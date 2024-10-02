1c1b9c1eedc1a41d810054ac34a5001a6f8b2dac
Sabarish <129573072+Sabarishjoc@users.noreply.github.com>
2024-10-02 11:01:15 +0000

    add

---
+++ Practice/day04/ad_cal.py
@@ -0,0 +1,25 @@
+# Function to add two numbers
+def add(a, b):
+    return a + b
+
+# Function to subtract two numbers
+def subtract(a, b):
+    return a - b
+
+# Function to multiply two numbers
+def multiply(a, b):
+    return a * b
+
+# Function to divide two numbers
+def divide(a, b):
+    return a / b
+
+# Predefined numbers
+num1 = 10
+num2 = 5
+
+# Arithmetic operations
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", multiply(num1, num2))
+print("Division:", divide(num1, num2))
---
+++ Practice/day04/calculator_one.py
@@ -0,0 +1,20 @@
+
+def addition(a, b):
+    add = a + b
+    return add
+
+def sub(a, b):
+    s = a - b
+    return s
+
+def mul(a, b):
+    m = a * b
+    return m
+
+x = addition(10, 20)
+y = sub(100, 20)
+z = mul(20, 10)
+
+print(x, y, z)
+
+
