019f1ecb2522326fa81301ab785b7345a875edfc
Sabarish <129573072+Sabarishjoc@users.noreply.github.com>
2024-10-25 07:36:10 +0000

    new

---
+++ Practice/day08/list.py
@@ -0,0 +1,14 @@
+ec2_list = ["ec2_1", "ec2_2","ec2_3", "ec2_4"]
+print(ec2_list)
+ec2_list.append("ec2_5")
+print(ec2_list)
+ec2_list.remove("ec2_2")
+print(ec2_list)
+print(ec2_list[3])
+print(len(ec2_list))
+ec2_list = ["ec2_1", "ec2_2","ec2_3", "ec2_4"]
+print(ec2_list[0:2])
+num = [30, 20, 10]
+num.sort()
+print(num)
+
