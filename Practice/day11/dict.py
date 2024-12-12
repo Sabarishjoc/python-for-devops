2a4abeb35d5993b21fad9e4304aad2b3c9281421
Sabarish <129573072+Sabarishjoc@users.noreply.github.com>
2024-12-12 11:58:08 +0000

    new

---
+++ Practice/day11/dict.py
@@ -0,0 +1,16 @@
+ec2_instances = [
+    {
+        "ec2_type": "t2.micro",
+        "id": "01"
+    },
+    {
+        "ec2_type": "t2.medium",
+        "id": "02"
+    },
+    {
+        "ec2_type": "t2.large",
+        "id": "03"
+    }
+]
+
+print(ec2_instances[0]["ec2_type"])
